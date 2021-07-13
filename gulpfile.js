0807e2b9f404c6d981e7230e8a6436acf028a9b1
Danil Zelenin <danil110980@mail.ru>
2021-07-13 19:48:27 +0500

    add img

--- gulpfile.js
+++ gulpfile.js
@@ -6,21 +6,21 @@ let path = {
         html: [source_folder + "/*.html", "!" + source_folder + "/_*.html"],
         css: source_folder + "/scss/style.scss",
         js: source_folder + "/js/script.js",
-        // img: source_folder + "/img/**/*.{jpg,png,svg,gif,ico,webp}",
+        img: source_folder + "/img/**/*.{jpg,png,svg,gif,ico,webp}",
         // fonts: source_folder + "/fonts/*.ttf",
     },
     build: {
         html: project_folder + "/",
         css: project_folder + "/css/",
         js: project_folder + "/js/",
-        // img: project_folder + "/img/",
+        img: project_folder + "/img/",
         // fonts: project_folder + "/fonts/",
     },
     watch: {
         html: source_folder + "/**/*.html",
         css: source_folder + "/scss/**/*.scss",
         js: source_folder + "/js/**/*.js",
-        // img: source_folder + "/img/**/*.{jpg,png,svg,gif,ico,webp}",
+        img: source_folder + "/img/**/*.{jpg,png,svg,gif,ico,webp}",
     },
     clean: "./" + project_folder + "/"
 }
@@ -36,6 +36,10 @@ let group_media = require('gulp-group-css-media-queries')
 let clean_css = require('gulp-clean-css')
 let rename = require('gulp-rename')
 let uglify = require('gulp-uglify-es').default
+let imagemin = require('gulp-imagemin')
+let webp = require('gulp-webp')
+let webphtml = require('gulp-webp-html')
+let webpcss = require('gulp-webpcss')
 
 
 function browserSync() {
@@ -48,11 +52,10 @@ function browserSync() {
     })
 }
 
-
-
 function html() {
     return src(path.src.html)
         .pipe(fileinclude())
+        .pipe(webphtml())
         .pipe(dest(path.build.html))
         .pipe(browsersync.stream())
 }
@@ -73,6 +76,7 @@ function css() {
                 cascade: true
             })
         )
+        .pipe(webpcss())
         .pipe(dest(path.build.css))
         .pipe(clean_css())
         .pipe(
@@ -100,23 +104,46 @@ function js() {
         .pipe(browsersync.stream())
 }
 
+function img() {
+    return src(path.src.img)
+        .pipe(
+            webp({
+                quality: 70
+            })
+        )
+        .pipe(dest(path.build.img))
+        .pipe(src(path.src.img))
+        .pipe(
+            imagemin({
+                progressive: true,
+                svgoPlugins: [{removeViewBox: false}],
+                interlaced: true,
+                optimizationLevel: 3
+            })
+        )
+        .pipe(dest(path.build.img))
+        .pipe(browsersync.stream())
+}
+
 function watchFiles() {
     gulp.watch([path.watch.html], html)
     gulp.watch([path.watch.css], css)
     gulp.watch([path.watch.js], js)
+    gulp.watch([path.watch.img], img)
 }
 
 function clean() {
     return del(path.clean)
 }
 
-let build = gulp.series(clean, gulp.parallel( js, css, html))
+let build = gulp.series(clean, gulp.parallel(img, js, css, html))
 let watch = gulp.parallel(build, watchFiles, browserSync)
 
 
 exports.html = html
 exports.css = css
 exports.js = js
+exports.img = img
 
 exports.build = build
 exports.watch = watch
